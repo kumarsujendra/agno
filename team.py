04c1b5a469d581e6155fb7dd12f9224b1c678ca7
Sujendra Kumar <sujendra.kumar@hcl.com>
2025-02-20 15:12:28 +0530

    #News

--- team.py
+++ team.py
@@ -37,15 +37,27 @@ finance_agent = Agent(
     markdown=True,
 )
 
+news_agent = Agent(
+    name="News Agent",
+    agent_id="news_agent",
+    role="Get breaking news",
+    model=OpenAIChat(id="gpt-4o"),
+    description="You are an enthusiastic news reporter with a flair for storytelling!",
+    instructions=["Always include sources"],
+    storage=SqliteAgentStorage(table_name="news_agent_session", db_file="agents.db"),
+    markdown=True
+)
+
 team_agent = Agent(
     name="Team Agent",
     agent_id="team_agent",
-    team=[web_agent, finance_agent],
+    team=[web_agent, finance_agent, news_agent],
     storage=SqliteAgentStorage(table_name="team_agent_session", db_file="agents.db"),
     markdown=True,
 )
 
-app = Playground(agents=[finance_agent, web_agent, team_agent]).get_app()
+#app = Playground(agents=[finance_agent, web_agent, team_agent]).get_app()
+app = Playground(agents=[web_agent, finance_agent, news_agent, team_agent]).get_app()
 
 if __name__ == "__main__":
     serve_playground_app("team:app", reload=True)
